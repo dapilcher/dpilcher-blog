8ff25be039f2763ec45b6f47df5a384c77c267c6
Doug Pilcher <dpilch1@gmail.com>
2017-09-06 16:17:17 -0500

    Add Post model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+
 # Create your models here.
+class Post(models.Model):
+    pub_date = models.DateTimeField(auto_now=True)
+    title = models.CharField(max_length=128)
+    body = models.TextField()
+
+    def __str__(self):
+        return self.title
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
+from .models import Post
+
 
 class HomePageView(TemplateView):
     def get(self, request, **kwargs):
-        return render(request, 'blog/index.html', context=None)
+        latest_posts = Post.objects.order_by('-pub_date')[:2]
+        context = {'latest_posts':latest_posts}
+        return render(request, 'blog/index.html', context=context)
